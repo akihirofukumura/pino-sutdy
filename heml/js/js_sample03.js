74015ef6b5c3602791fc4973a8b92b9f6b3f4b12
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2022-11-01 20:06:05 +0900

    true false

--- heml/js/js_sample03.js
+++ heml/js/js_sample03.js
@@ -1,16 +1,25 @@
 const container = document.getElementById("container");
 const box = document.getElementById("box");
 
+let isMauseDown = false;
+
+box.addEventListener("mousedown",function(){
+  isMauseDown = true;
+})
+
+box.addEventListener("mouseup",function(){
+  isMauseDown = false;
+})
+
 console.log(container);
 console.log(box);
-
-// box.style.backgroundColor = "tomato";
 
 container.addEventListener("mousemove",function(e){
   const x = e.clientX
   const y = e.clientY
-  console.log(x,y);
-  box.style.top = `${y}px`;
-  box.style.left =`${x}px`;
+  
+
+  box.style.top = `${y-50}px`;
+  box.style.left =`${x-50}px`;
 })
 
