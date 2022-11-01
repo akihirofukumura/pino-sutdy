93c8be38ce2494bd353735f8383177cc1e8e03ed
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2022-11-01 19:51:12 +0900

    動いた

--- heml/js/js_sample03.js
+++ heml/js/js_sample03.js
@@ -10,7 +10,7 @@ container.addEventListener("mousemove",function(e){
   const x = e.clientX
   const y = e.clientY
   console.log(x,y);
-  box.style.top = y.toString();
-  box.style.left = x.toString();
+  box.style.top = `${y}px`;
+  box.style.left =`${x}px`;
 })
 
