02735770e55776c03cabc4958105e5e650f5e88a
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2022-11-01 19:43:52 +0900

    座標を取った

--- heml/js/js_sample03.js
+++ heml/js/js_sample03.js
@@ -1,1 +1,16 @@
-console.log("aaa");
+const container = document.getElementById("container");
+const box = document.getElementById("box");
+
+console.log(container);
+console.log(box);
+
+// box.style.backgroundColor = "tomato";
+
+container.addEventListener("mousemove",function(e){
+  const x = e.clientX
+  const y = e.clientY
+  console.log(x,y);
+  box.style.top = y.toString();
+  box.style.left = x.toString();
+})
+
