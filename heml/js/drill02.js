e1b094f1abd23b19b75bfd38accfd5b4095aefc7
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2022-12-13 20:06:52 +0900

    drill1~2（途中）

---
+++ heml/js/drill02.js
@@ -0,0 +1,21 @@
+/*
+drill02 - 配列
+配列 arr を作成し、以下の出力になる様にデータを変更すること
+
+[表示内容]
+[]
+[200]
+[200,300]
+[300]
+*/
+
+let arr = [];
+
+console.log(arr);
+arr.push(200);
+console.log(arr);
+arr.push(300);
+
+// 配列を消すのを書く
+console.lo;
+console.log(arr);
